deeb9c36c79c1afa2c0255e271022f3a6e1bba1d
YesnielX <yesnielviv2@gmail.com>
2022-09-30 17:06:36 -0400

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 export default function Home() {
-
-  return <Redirect to="https://yesnielx.github.io/auto-translate-docs/docs" />;
+  return <Redirect to="docs/" />;
 }
