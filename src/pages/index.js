9cb8fc8d6f384899236641b20d95231d63479ad3
YesnielX <yesnielviv2@gmail.com>
2022-09-30 16:56:38 -0400

    fix(index.js): update redirect to docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 export default function Home() {
-  return <Redirect to="/docs/" />;
+  const homeDocs = `${location.hostname}/auto-translate-docs/docs`;
+
+  return <Redirect to={homeDocs} />;
 }
