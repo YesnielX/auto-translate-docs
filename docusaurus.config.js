25bc34b598051726ce40bdc0016e7d23e689b565
YesnielX <yesnielviv2@gmail.com>
2022-09-30 14:55:11 -0400

    fix(docusaurus.config.js): fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Auto Translate Docs",
   tagline: "Auto Translate | Discord Bot",
-  url: "https://autotranslatedocs.github.io",
-  baseUrl: "/",
+  url: "https://github.com",
+  baseUrl: "/auto-translate-docs",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "YesnielX", // Usually your GitHub org/user name.
   projectName: "auto-translate-docs", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
